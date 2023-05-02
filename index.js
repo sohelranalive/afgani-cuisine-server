61719274834c72956b503b6f34c982cd1674c1d8
sohelranalive <sohel.cse37@gmail.com>
2023-05-02 08:42:36 +0600

    CORS installed and implement

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const data = { name: 'Sohel', age: 23 }
+
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(data)
 })
 
 app.listen(port, () => {
