11e6b750f7786fc2cc2e151fdbb010bf7794288b
sohelranalive <sohel.cse37@gmail.com>
2023-05-02 10:10:07 +0600

    json data added

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 
-const data = { name: 'Sohel', age: 23 }
+
+const chefsInfo = require('./data/chefsInfo.json')
+const cuisineInfo = require('./data/cuisineInfo.json')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send(data)
+    res.send(chefsInfo)
+})
+
+app.get('/cuisine', (req, res) => {
+    res.send(cuisineInfo)
+})
+
+app.get('/cuisine/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCuisine = cuisineInfo.filter(cuisine => cuisine.chefsId == id)
+    res.send(selectedCuisine)
 })
 
 app.listen(port, () => {
